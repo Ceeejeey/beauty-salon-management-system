acb409ed4f04eaed79a0409b736e873b400c638a
RakeshWijesingha <165076456+RakeshWijesingha@users.noreply.github.com>
2025-08-03 18:52:20 +0530

    update

--- frontend/src/components/customer/Sidebar.jsx
+++ frontend/src/components/customer/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = ({ isSidebarOpen, setIsSidebarOpen, setActiveComponent }) => {
 
   return (
     <aside
-      className={`fixed top-16 bottom-0 w-64 bg-gradient-to-br from-pink-50 to-white shadow-xl transform lg:transform-none ${
+      className={`fixed top-25 bottom-0 w-64 bg-gradient-to-br from-pink-50 to-white shadow-xl transform lg:transform-none ${
         isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
       } transition-transform duration-300 z-20 overflow-y-auto`}
     >
@@ -53,7 +53,7 @@ const Sidebar = ({ isSidebarOpen, setIsSidebarOpen, setActiveComponent }) => {
         `}
       </style>
       <div className="p-10 font-poppins sidebar-scroll">
-        <nav className="space-y-3">
+        <nav className="space-y-5">
           {navItems.map((item) => (
             <div key={item.name}>
               {item.dropdown ? (
